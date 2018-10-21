33beece004b368229ee35b37bf546149912645d6
memolop10 <inglopezvargas@gmail.com>
2018-10-21 15:33:49 -0500

    misjs

---
+++ js/ejercicios_funciones.js
@@ -0,0 +1,15 @@
+
+
+
+
+var numero1 = prompt('ingresa primer numero');
+
+var numero2 = prompt('ingresa segundo numero');
+
+
+function suma_y_muestra() {
+    resultado = numero1 + numero2;
+    console.log("El resultado es " + resultado);
+  }
+   
+  suma_y_muestra();
